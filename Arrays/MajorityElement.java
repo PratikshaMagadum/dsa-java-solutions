bf86fb96efaf7b9e9664c4ed6c006787d2b90d09
Pratiksha Magdum <142082094+PratikshaMagadum@users.noreply.github.com>
2026-05-11 21:38:27 +0530

    O(n) O(1)-space Majority Element solution using Boyer-Moore Voting
    Implemented optimal solution using Boyer-Moore Voting Algorithm.
    Time Complexity: O(n), Space Complexity: O(1).

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       
+       int candidate=0;
+       int count=0;
+
+       for(int i:nums) {
+
+          if(count==0){
+              candidate=i;
+          }
+
+          if(candidate==i){
+              count++;
+          }
+          else{
+              count--;
+          }
+         
+       }
+      
+        return candidate;       
+    }
+}
